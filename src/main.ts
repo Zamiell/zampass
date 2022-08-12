a4e02b3c3ad7efcf091a6e024d1a9fc45e7792f1
James <5511220+Zamiell@users.noreply.github.com>
2022-08-12 18:40:20 -0400

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,52 @@
+#!/usr/bin/env node
+
+import clipboardy from "clipboardy";
+import sourceMapSupport from "source-map-support";
+import { getRandomArrayElement, initArray, repeat } from "./util.js";
+
+const DEFAULT_LENGTH = 128;
+
 main();
 
-function main() {}
+function main() {
+  sourceMapSupport.install();
+
+  const password = getPassword(DEFAULT_LENGTH);
+  clipboardy.writeSync(password);
+  console.log("Generated a password and stored it in the clipboard.");
+}
+
+function getPassword(length: number): string {
+  const validCharacters = getValidCharacters();
+
+  let password = "";
+  repeat(length, () => {
+    const randomCharacter = getRandomArrayElement(validCharacters);
+    password += randomCharacter;
+  });
+
+  return password;
+}
+
+function getValidCharacters(): string[] {
+  const lowercaseLetters = getCharactersBetween("a", "z");
+  const uppercaseLetters = getCharactersBetween("A", "Z");
+  const numbers = getCharactersBetween("0", "9");
+
+  return [...lowercaseLetters, ...uppercaseLetters, ...numbers];
+}
+
+function getCharactersBetween(
+  firstCharacter: string,
+  lastCharacter: string,
+): string[] {
+  const firstCharacterCode = firstCharacter.charCodeAt(0);
+  const lastCharacterCode = lastCharacter.charCodeAt(0);
+  const arrayLength = lastCharacterCode - firstCharacterCode;
+  const firstCharacterCodes = initArray(arrayLength, firstCharacterCode);
+  const characterCodes = firstCharacterCodes.map(
+    (_element, i) => firstCharacterCode + i,
+  );
+
+  return characterCodes.map((element) => String.fromCharCode(element));
+}
